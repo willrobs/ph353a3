c9fcf82bc327457ea6fc4a083def820d04e5b5fa
willrobs <will.roberts.223@gmail.com>
2017-05-06 19:16:48 +0100

    Add files via upload

--- reweight.sh
+++ reweight.sh
@@ -3,16 +3,21 @@ points=100
 a=0.05
 
 mass=2
+mu=10
 bootsamp=35
 eecorrdist=15
 mprime=2
 
-interval=2
-upper=2
-lower=2
+interval_mu=1 # check these parameters are definitely correct before running
+upper_mu=0
+lower_mu=0
 
-for mu in {5..40..5}
+interval_m=1
+upper_m=1
+lower_m=0
+
+for m in {10..10..2}
 do
-  python reweight.py $niter $points $a $eecorrdist $bootsamp $interval $upper $lower $mass $mu
+  python reweight.py $niter $points $a $eecorrdist $bootsamp $interval_mu $upper_mu $lower_mu $interval_m $upper_m $lower_m $m $mu
 done
 
