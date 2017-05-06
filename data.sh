a22a78359bfa49c3bb3832f7707627a1ca7b3d91
willrobs <will.roberts.223@gmail.com>
2017-05-06 19:16:32 +0100

    Add files via upload

--- data.sh
+++ data.sh
@@ -1,25 +1,25 @@
 niter=10000
 points=100
 a=0.05
-step=0.25
+step=0.18
 ttime=1000
 
-mass=2
+m=2
 mu=10
 
 corrdist=500
 bootsamp=75
 eecorrdist=15
 
-for mu in {5..40..5}
+for m in {2..10..2}
   do
 
-    python data.py $niter $points $a $step $ttime $mass $mu $anharm
+    python data.py $niter $points $a $step $ttime $m $mu $anharm
 
-    python bin.py $niter $points $a $corrdist $mass $mu
+    python bin.py $niter $points $a $corrdist $m $mu
 
-    python gse.py $niter $points $a $bootsamp $mass $mu
+    python gse.py $niter $points $a $bootsamp $m $mu
 
-    python fee.py $niter $points $a $eecorrdist $bootsamp $mass $mu
+    python fee.py $niter $points $a $eecorrdist $bootsamp $m $mu
 
   done
